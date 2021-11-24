d561633c23ba4716f1ddd52f461e652d3e20d23f
Max Deisadze <m.deisadze@hotmail.com>
2021-11-23 19:25:38 -0500

    created delete method to delete faculty from db

--- faculty_window_flow.py
+++ faculty_window_flow.py
@@ -10,6 +10,7 @@ import gui
 import popup
 import sys
 
+#Test Commit demo
 class FacultyMainWindow(QWidget):
     def __init__(self):
         super(FacultyMainWindow, self).__init__()
@@ -18,8 +19,6 @@ class FacultyMainWindow(QWidget):
         self.add_faculty_button = QPushButton(self)
         self.update_faculty_info_button = QPushButton(self)
         self.delete_faculty_button = QPushButton(self)
-        #self.access_faculty_button = QPushButton(self)
-
         self.back_button = QPushButton(self)
 
         self.setup_ui()
@@ -52,11 +51,6 @@ class FacultyMainWindow(QWidget):
         self.delete_faculty_button.move(355, 300)
         self.delete_faculty_button.setText('Delete')
 
-        # # Access Button
-        # self.access_faculty_button.resize(100, 50)
-        # self.access_faculty_button.move(355, 300)
-        # self.access_faculty_button.setText('Access')
-
         # Back Button
         self.back_button.resize(50, 35)
         self.back_button.move(30, 30)
@@ -64,22 +58,25 @@ class FacultyMainWindow(QWidget):
         self.back_button.setFont(QFont('Papyrus', 7, QFont.Bold))
 
         # Signals/Slots
-        self.add_faculty_button.clicked.connect(self.faculty_id_being_added_window)
-        # self.update_faculty_info_button.clicked.connect(self.faculty_id_being_updated_pop_up)
-        # self.delete_faculty_button.clicked.connect(self.faculty_id_being_deleted_pop_up)
-        # self.access_faculty_button.clicked.connect(self.faculty_id_being_accessed_pop_up)
+        self.update_faculty_info_button.clicked.connect(self.update_button_clicked)
+        self.delete_faculty_button.clicked.connect(self.delete_button_clicked)
         self.back_button.clicked.connect(self.back_button_clicked)
+        self.add_faculty_button.clicked.connect(self.faculty_id_being_added_window)
 
     def back_button_clicked(self):
         self.previous_window = gui.MainWindow()
         self.previous_window.show()
         self.close()
 
-    # def faculty_id_being_deleted_pop_up(self):
-    #     self.popup = popup.DeleteFacultyPopUp()
-    #
-    # def faculty_id_being_updated_pop_up(self):
-    #     self.popup = popup.UpdateFacultyPopUp()
+    def update_button_clicked(self):
+        self.faculty_update_window = FacultyUpdateWindow()
+        self.faculty_update_window.show()
+        self.close()
+
+    def delete_button_clicked(self):
+        self.delete_faculty_window = DeleteFacultyWindow()
+        self.delete_faculty_window.show()
+        self.close()
 
     def faculty_id_being_added_window(self):
         self.window = AddFacultyWindow()
@@ -87,6 +84,171 @@ class FacultyMainWindow(QWidget):
         self.close()
 
 
+class FacultyUpdateWindow(QWidget):
+    def __init__(self):
+        super(FacultyUpdateWindow, self).__init__()
+        self.title = QLabel(self)
+        self.update_faculty_info_button = QPushButton(self)
+        self.input_box = QLineEdit(self)
+        self.back_button = QPushButton(self)
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        # Window
+        self.resize(600, 500)
+        window_width = int(self.frameGeometry().width())
+        self.setWindowTitle("Update Faculty Window")
+
+        # Title
+        self.title.setText("Enter Faculty ID")
+        self.title.setFont(QFont('Papyrus', 16, QFont.Bold))
+        self.title.adjustSize()
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.title.move(int((window_width - self.title.width()) / 2), 100)
+
+        # Input Box
+        self.input_box.resize(200, 30)
+        self.input_box.move(200, 200)
+
+        # Update Faculty Button
+        self.update_faculty_info_button.resize(100, 50)
+        self.update_faculty_info_button.move(245, 300)
+        self.update_faculty_info_button.setText('Access faculty')
+
+        # Back Button
+        self.back_button.resize(50, 35)
+        self.back_button.move(30, 30)
+        self.back_button.setText('Back')
+        self.back_button.setFont(QFont('Papyrus', 7, QFont.Bold))
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+        # Signals/Slots
+        self.update_faculty_info_button.clicked.connect(self.access_faculty_clicked)
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+    def back_button_clicked(self):
+        self.previous_window = FacultyMainWindow()
+        self.previous_window.show()
+        self.close()
+
+    def access_faculty_clicked(self):
+        self.faculty_update_window = AccessedFacultyUpdatedWindow()
+        self.faculty_update_window.show()
+        self.close()
+
+
+class AccessedFacultyUpdatedWindow(QWidget):
+    def __init__(self):
+        super(AccessedFacultyUpdatedWindow, self).__init__()
+
+        self.title = QLabel(self)
+        self.update_accessed_faculty_button = QPushButton(self)
+        self.input_box = QLineEdit(self)
+        self.input_box2 = QLineEdit(self)
+        self.back_button = QPushButton(self)
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        # Window
+        self.resize(600, 500)
+        window_width = int(self.frameGeometry().width())
+        self.setWindowTitle("Update faculty Window")
+
+        # Title
+        self.title.setText("Enter New Faculty ID:")
+        self.title.setFont(QFont('Papyrus', 16, QFont.Bold))
+        self.title.adjustSize()
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.title.move(int((window_width - self.title.width()) / 2), 100)
+
+        # Input Box
+        self.input_box.resize(200, 30)
+        self.input_box.move(200, 200)
+
+        # Input Box 2
+        self.input_box2.resize(200, 30)
+        self.input_box2.move(200, 250)
+
+        # Update Button
+        self.update_accessed_faculty_button.resize(100, 50)
+        self.update_accessed_faculty_button.move(245, 300)
+        self.update_accessed_faculty_button.setText("Update")
+
+        # Back Button
+        self.back_button.resize(50, 35)
+        self.back_button.move(30, 30)
+        self.back_button.setText('Back')
+        self.back_button.setFont(QFont('Papyrus', 7, QFont.Bold))
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+        #Signals / Slots
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+    def back_button_clicked(self):
+        self.previous_window = FacultyUpdateWindow()
+        self.previous_window.show()
+        self.close()
+
+
+
+class DeleteFacultyWindow(QWidget):
+    def __init__(self):
+        super(DeleteFacultyWindow, self).__init__()
+
+        self.title = QLabel(self)
+        self.delete_faculty_button = QPushButton(self)
+        self.input_box = QLineEdit(self)
+        self.back_button = QPushButton(self)
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        # Window
+        self.resize(600, 500)
+        window_width = int(self.frameGeometry().width())
+        self.setWindowTitle("Delete faculty Window")
+
+        # Title
+        self.title.setText("Enter Faculty ID")
+        self.title.setFont(QFont('Papyrus', 16, QFont.Bold))
+        self.title.adjustSize()
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.title.move(int((window_width - self.title.width()) / 2), 100)
+
+        # Input Box
+        self.input_box.resize(200, 30)
+        self.input_box.move(200, 200)
+
+        # Delete faculty Button
+        self.delete_faculty_button.resize(100, 50)
+        self.delete_faculty_button.move(245, 300)
+        self.delete_faculty_button.setText("Delete")
+
+        # Back Button
+        self.back_button.resize(50, 35)
+        self.back_button.move(30, 30)
+        self.back_button.setText('Back')
+        self.back_button.setFont(QFont('Papyrus', 7, QFont.Bold))
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+
+        #Signals/Slots
+        self.delete_faculty_button.clicked.connect(self.delete_faculty_button_clicked)
+        self.back_button.clicked.connect(self.back_button_clicked)
+
+    def back_button_clicked(self):
+        self.previous_window = FacultyMainWindow()
+        self.previous_window.show()
+        self.close()
+
+    def delete_faculty_button_clicked(self):
+        conn = sqlite3.connect('north_star_school_database.db')
+        cur = conn.cursor()
+        cur.execute("""DELETE FROM instructor WHERE instructor_id = ? """, (self.input_box.text(),))
+        conn.commit()
+        conn.close()
 
 class AddFacultyWindow(QWidget):
     def __init__(self):
@@ -149,4 +311,3 @@ class AddFacultyWindow(QWidget):
         self.previous_window = FacultyMainWindow()
         self.previous_window.show()
         self.close()
-
