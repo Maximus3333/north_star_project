09bafaf041ba8ca031c190dfbc7377b90b01b461
Max Deisadze <m.deisadze@hotmail.com>
2021-11-27 11:24:03 -0500

    Added to popup.py

--- popup.py
+++ popup.py
@@ -1,26 +1,56 @@
 # File Is for Creating popups that throw ERRORS!
-from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
 class ErrorPopUp(QDialog):
-    def __init__(self):
+    def __init__(self, error_type):
         super(ErrorPopUp, self).__init__()
 
-
+        self.error_type = error_type
         self.setup_pop_up_box()
 
     def setup_pop_up_box(self):
-
-        self.resize(330, 140)
-
         self.title = QLabel(self)
 
-        self.title.setText('Error Adding to the Databse')
+        self.title.setText(self.error_type + "!")
         self.title.adjustSize()
-        print(self.title.height())
-        self.title.move(5, 20)
+        self.title.setAlignment(Qt.AlignCenter)
+        self.title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.title, 0, 0)
+
+        self.setLayout(self.layout)
 
         self.window_title = 'Error!'
+        self.setWindowTitle(self.window_title)
+
+        self.show()
+
+
+class SuccessPopUp(QDialog):
+    def __init__(self, success_type):
+        super(SuccessPopUp, self).__init__()
+
+        self.success_type = success_type
+        self.setup_pop_up_box()
+
+    def setup_pop_up_box(self):
+        self.title = QLabel(self)
+
+        self.title.setText(self.success_type + "!")
+        self.title.adjustSize()
+        self.title.setAlignment(Qt.AlignCenter)
+        self.title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.title, 0, 0)
+
+        self.setLayout(self.layout)
+
+        self.window_title = 'Success!'
         self.setWindowTitle(self.window_title)
 
         self.show()
